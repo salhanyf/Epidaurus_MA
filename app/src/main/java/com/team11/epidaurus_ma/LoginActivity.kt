b5a613607576b6f1c26ec84c420cd42280d43777
Farah <farah.salhany@hotmail.com>
2024-01-28 22:16:59 -0500

    fixed the sign in crash closes #35

--- app/src/main/java/com/team11/epidaurus_ma/LoginActivity.kt
+++ app/src/main/java/com/team11/epidaurus_ma/LoginActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.github.jan.supabase.SupabaseClient
@@ -34,12 +36,11 @@ class LoginActivity : AppCompatActivity(), CoroutineScope {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // Sign up redirect btn
+        val emailET = findViewById<EditText>(R.id.emailSignIn)
+        val pswET = findViewById<EditText>(R.id.passwordSignIn)
+        val signInBtn = findViewById<Button>(R.id.SignInButton)
         val signUpBtn = findViewById<Button>(R.id.signUpRedirect)
-        signUpBtn.setOnClickListener{
-            val signUnIntent = Intent (this, SignUpActivity::class.java)
-            startActivity(signUnIntent)
-        }
+        val errorMessageTV = findViewById<TextView>(R.id.errorMessage)
 
         val supabase = createSupabaseClient(
             "https://faafgdjvgcpjbchmbmhg.supabase.co",
@@ -48,57 +49,85 @@ class LoginActivity : AppCompatActivity(), CoroutineScope {
             install(Auth)
         }
 
-        val signInBtn = findViewById<Button>(R.id.SignInButton)
+        // Sign up redirect btn
+        signUpBtn.setOnClickListener{
+            val signUnIntent = Intent (this, SignUpActivity::class.java)
+            startActivity(signUnIntent)
+        }
+
+        // Sign in btn
         signInBtn.setOnClickListener{
-            val emailET = findViewById<EditText>(R.id.emailSignIn)
-            val pswET = findViewById<EditText>(R.id.passwordSignIn)
             val emailStr = emailET.text.toString()
             val pswStr = pswET.text.toString()
-            if(emailStr == "" || pswStr == ""||emailStr == null || pswStr == null){
-               Toast.makeText(this, "Credentials are empty", Toast.LENGTH_SHORT).show()
-            }else{
+            emailET.setBackgroundResource(R.drawable.outlined_box_green)
+            pswET.setBackgroundResource(R.drawable.outlined_box_green)
+            errorMessageTV.visibility= View.GONE
+
+            var isValid = true
+
+            if(emailStr.isEmpty()){
+                emailET.setBackgroundResource(R.drawable.outlined_box_red)
+                emailET.error = "Email must not be empty"
+                isValid = false
+            }
+
+            if (pswStr.isEmpty()) {
+                pswET.setBackgroundResource(R.drawable.outlined_box_red)
+                pswET.error = "Password must not be empty"
+                isValid = false
+            }
+
+            if (!isValid) {
+                // if one or both fields are empty
+
+            } else{
+                // if both fields are filled, attempt to sign in
                 launch{
                     signIn(supabase,emailStr,pswStr)
                 }
             }
-
         }
     }
 
-    suspend fun signIn(supabase: SupabaseClient, emailStr:String?, pswStr:String?){
-
-        supabase.auth.signInWith(Email){
-            if (emailStr != null) {
-                email = emailStr
-            }
-            if (pswStr != null) {
-                password = pswStr
-            }
-        }
-        supabase.auth.sessionStatus.collect{
-            when(it){
-                is SessionStatus.Authenticated -> {
-                    Toast.makeText(this, "Sign in success", Toast.LENGTH_SHORT).show()
-                    val metadata = supabase.auth.retrieveUserForCurrentSession()?.userMetadata
-                    val homePageIntent = Intent(this, HomePageActivity::class.java)
-                    homePageIntent.putExtra("nurseMetadata", metadata.toString())
-                    supabase.close()
-                    startActivity(homePageIntent)
-
+    private suspend fun signIn(supabase: SupabaseClient, emailStr:String?, pswStr:String?){
+        try {
+            supabase.auth.signInWith(Email) {
+                if (emailStr != null) {
+                    this.email = emailStr
                 }
-                SessionStatus.LoadingFromStorage -> {
-                    Toast.makeText(this, "Loading from Storage", Toast.LENGTH_SHORT).show()
-                    supabase.close()
-                }
-                SessionStatus.NetworkError -> {
-                    Toast.makeText(this, "Network Error", Toast.LENGTH_SHORT).show()
-                    supabase.close()
-                }
-                SessionStatus.NotAuthenticated -> {
-                    Toast.makeText(this, "Sign in failure", Toast.LENGTH_SHORT).show()
-                    supabase.close()
+                if (pswStr != null) {
+                    this.password = pswStr
                 }
             }
+            supabase.auth.sessionStatus.collect{
+                when(it){
+                    is SessionStatus.Authenticated -> {
+                        Toast.makeText(this, "Sign in success", Toast.LENGTH_SHORT).show()
+                        val metadata = supabase.auth.retrieveUserForCurrentSession()?.userMetadata
+                        val homePageIntent = Intent(this, HomePageActivity::class.java)
+                        homePageIntent.putExtra("nurseMetadata", metadata.toString())
+                        supabase.close()
+                        startActivity(homePageIntent)
+                    }
+                    SessionStatus.LoadingFromStorage -> {
+                        Toast.makeText(this, "Loading from Storage", Toast.LENGTH_SHORT).show()
+                        supabase.close()
+                    }
+                    SessionStatus.NetworkError -> {
+                        Toast.makeText(this, "Network Error", Toast.LENGTH_SHORT).show()
+                        supabase.close()
+                    }
+                    SessionStatus.NotAuthenticated -> {
+                        Toast.makeText(this, "Authenticated Failed", Toast.LENGTH_SHORT).show()
+                        supabase.close()
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            val errorMessageTV = findViewById<TextView>(R.id.errorMessage)
+            errorMessageTV.visibility= View.VISIBLE
+            errorMessageTV.text = getString(R.string.invalid_email_pass)
+            e.printStackTrace()
         }
     }
 }
