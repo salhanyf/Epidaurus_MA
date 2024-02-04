e81ea4f5f4cc1cd49e9f686a9c118b040b5f8cb0
Farah <farah.salhany@hotmail.com>
2024-02-04 16:36:22 -0500

    profile updates now reflect on the database

--- app/src/main/java/com/team11/epidaurus_ma/ProfileActivity.kt
+++ app/src/main/java/com/team11/epidaurus_ma/ProfileActivity.kt
@@ -13,20 +13,37 @@ import android.widget.ImageView
 import android.widget.Button
 import android.widget.GridView
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.gotrue.auth
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.put
 import org.json.JSONObject
 import kotlin.coroutines.CoroutineContext
 import kotlin.math.max
 
 class ProfileActivity : AppCompatActivity(), CoroutineScope {
+
+    private val supabase = createSupabaseClient(
+        "https://faafgdjvgcpjbchmbmhg.supabase.co",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhYWZnZGp2Z2NwamJjaG1ibWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM2MTg4NzAsImV4cCI6MjAxOTE5NDg3MH0.aqg8lSD7tQrWhXjfZi7OiRJOEF1ArG-wdRd9KauvZPU"
+    ){
+        install(Auth)
+    }
     companion object {
         private const val PREFS_NAME = "ProfilePrefs"
         private const val AVATAR_KEY = "avatar_resource_id"
     }
     private var selectedAvatarResId: Int = R.drawable.avatar_4
+    private var originalName: String? = null
+    private var originalDep: String? = null
+    private var originalFloor: String? = null
     private var departmentId: String = "0"
     private var floorId: String = "0"
     private var job: Job = Job()
@@ -35,6 +52,12 @@ class ProfileActivity : AppCompatActivity(), CoroutineScope {
     override fun onDestroy() {
         super.onDestroy()
         job.cancel()
+        launch{
+            supabase.close()
+        }
+    }
+    override fun onResume() {
+        super.onResume()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,7 +73,13 @@ class ProfileActivity : AppCompatActivity(), CoroutineScope {
         val cancelBtn = findViewById<Button>(R.id.cancelButton)
         val saveBtn = findViewById<Button>(R.id.saveButton)
 
-        // Setup spinners
+        launch {
+            fetchUserData(supabase, nameEditText, departmentSpinner, floorSpinner)
+        }
+
+        // Spinners initially disabled
+        departmentSpinner.isEnabled = false
+        floorSpinner.isEnabled = false
         setupSpinner(departmentSpinner, R.array.department_array, "Select Department") { selected, position ->
             departmentId = when (selected) {
                 "Cardiology" -> "Cardiology"
@@ -69,23 +98,6 @@ class ProfileActivity : AppCompatActivity(), CoroutineScope {
             }
         }
 
-        var metadataJSON: JSONObject? = null
-        if (intent.hasExtra("nurseMetadata")) {
-            val nurseMetadataString = intent.getStringExtra("nurseMetadata")
-            metadataJSON = JSONObject(nurseMetadataString.toString())
-            nameEditText.setText(metadataJSON?.get("name").toString())
-
-            val departmentValue = metadataJSON?.getString("Department ID") ?: ""
-            val departmentIndex = resources.getStringArray(R.array.department_array).indexOf(departmentValue)
-            departmentSpinner.setSelection(max(departmentIndex, 0))
-
-            val floorValueFromJson = metadataJSON?.getString("Floor ID") ?: ""
-            val floorValue = if (floorValueFromJson.all { it.isDigit() }) "Floor $floorValueFromJson" else floorValueFromJson
-            val floorIndex = resources.getStringArray(R.array.floor_array).indexOf(floorValue)
-            floorSpinner.setSelection(max(floorIndex, 0))
-            Log.d("ProfileActivity", "Department: $departmentIndex , Floor: $floorIndex")
-        }
-
         val backButton = findViewById<ImageView>(R.id.backButton)
         backButton.setOnClickListener {
             finish()
@@ -97,22 +109,32 @@ class ProfileActivity : AppCompatActivity(), CoroutineScope {
         avatarImage.setImageResource(selectedAvatarResId)
 
         editBtn.setOnClickListener{
+            changeAvatarBtn.visibility = View.VISIBLE
             nameEditText.isEnabled = true
             nameEditText.setBackgroundResource(R.drawable.outlined_box_edit)
-            nameEditText.requestFocus()
-            changeAvatarBtn.visibility = View.VISIBLE
+            departmentSpinner.isEnabled = true
+            departmentSpinner.setBackgroundResource(R.drawable.outlined_box_edit)
+            floorSpinner.isEnabled = true
+            floorSpinner.setBackgroundResource(R.drawable.outlined_box_edit)
+
             editBtn.visibility = View.GONE
             cancelBtn.visibility = View.VISIBLE
             saveBtn.visibility = View.VISIBLE
 
+            nameEditText.requestFocus()
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.showSoftInput(nameEditText, InputMethodManager.SHOW_IMPLICIT)
         }
 
         cancelBtn.setOnClickListener{
+            changeAvatarBtn.visibility = View.GONE
             nameEditText.isEnabled = false
             nameEditText.setBackgroundResource(R.drawable.outlined_box_green)
-            changeAvatarBtn.visibility = View.GONE
+            departmentSpinner.isEnabled = false
+            departmentSpinner.setBackgroundResource(R.drawable.outlined_box_green)
+            floorSpinner.isEnabled = false
+            floorSpinner.setBackgroundResource(R.drawable.outlined_box_green)
+
             editBtn.visibility = View.VISIBLE
             cancelBtn.visibility = View.GONE
             saveBtn.visibility = View.GONE
@@ -120,23 +142,119 @@ class ProfileActivity : AppCompatActivity(), CoroutineScope {
 
         saveBtn.setOnClickListener{
             // Save the selected avatar resource ID
+            changeAvatarBtn.visibility = View.GONE
             sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
             with(sharedPreferences.edit()) {
                 putInt(AVATAR_KEY, selectedAvatarResId)
                 apply()
             }
+            // Save the new name
             nameEditText.isEnabled = false
             nameEditText.setBackgroundResource(R.drawable.outlined_box_green)
-            changeAvatarBtn.visibility = View.GONE
+            val currentName = nameEditText.text.toString()
+            if (originalName != currentName) {
+                updateNameInDatabase(supabase, currentName)
+                nameEditText.setText(currentName)
+            }
+
+            // Save the new department
+            departmentSpinner.isEnabled = false
+            updateDepInDatabase(supabase, departmentId)
+            departmentSpinner.setBackgroundResource(R.drawable.outlined_box_green)
+
+            // Save the new floor
+            floorSpinner.isEnabled = false
+            updateFloorInDatabase(supabase, floorId)
+            floorSpinner.setBackgroundResource(R.drawable.outlined_box_green)
+
             editBtn.visibility = View.VISIBLE
             cancelBtn.visibility = View.GONE
             saveBtn.visibility = View.GONE
-            //TODO:SAVE NAME IF CHANGED
+
         }
 
         changeAvatarBtn.setOnClickListener {
             if (nameEditText.isEnabled) {
                 showAvatarPickerDialog()
+            }
+        }
+    }
+
+    private suspend fun fetchUserData(
+        supabase: SupabaseClient,
+        nameEditText: EditText,
+        departmentSpinner: Spinner,
+        floorSpinner: Spinner
+    ) {
+        val metadata = supabase.auth.retrieveUserForCurrentSession()?.userMetadata.toString()
+        val metadataJSON: JSONObject?
+        metadataJSON = JSONObject(metadata)
+
+        originalName = metadataJSON.get("name").toString()
+        nameEditText.setText(originalName)
+
+        originalDep = metadataJSON.getString("Department ID") ?: ""
+        val departmentIndex = resources.getStringArray(R.array.department_array).indexOf(originalDep)
+        departmentSpinner.setSelection(max(departmentIndex, 0))
+
+        originalFloor = metadataJSON.get("Floor ID").toString()
+        val floorValueFromJson = metadataJSON.getString("Floor ID") ?: ""
+        val floorValue = if (floorValueFromJson.all { it.isDigit() }) "Floor $floorValueFromJson" else floorValueFromJson
+        val floorIndex = resources.getStringArray(R.array.floor_array).indexOf(floorValue)
+        floorSpinner.setSelection(max(floorIndex, 0))
+        Log.d("ProfileActivity", "Department: $originalDep , Floor: $originalFloor")
+    }
+
+    private fun updateNameInDatabase(supabase: SupabaseClient, newName: String) {
+        launch {
+            try {
+                val user = supabase.auth.modifyUser {
+                    data {
+                        put("name", newName)
+                    }
+                }
+                // Update successful
+                Log.d("ProfileActivity", "Name updated to: $newName")
+                Toast.makeText(this@ProfileActivity, "Name updated successfully", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Log.e("ProfileActivity", "Error updating name", e)
+                Toast.makeText(this@ProfileActivity, "Error updating name", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateDepInDatabase(supabase: SupabaseClient, newDep: String) {
+        launch {
+            try {
+                val user = supabase.auth.modifyUser {
+                    data {
+                        put("Department ID", newDep)
+                    }
+                }
+                // Update successful
+                Log.d("ProfileActivity", "Department updated to: $newDep")
+                //Toast.makeText(this@ProfileActivity, "Department updated successfully", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Log.e("ProfileActivity", "Error updating department", e)
+                //Toast.makeText(this@ProfileActivity, "Error updating department", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateFloorInDatabase(supabase: SupabaseClient, newFloor: String) {
+        launch {
+            try {
+                val user = supabase.auth.modifyUser {
+                    data {
+                        put("Floor ID",newFloor)
+                    }
+                }
+                // Update successful
+                Log.d("ProfileActivity", "Floor updated to: $newFloor")
+                //Toast.makeText(this@ProfileActivity, "Floor updated successfully", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Log.e("ProfileActivity", "Error updating floor", e)
+                //Toast.makeText(this@ProfileActivity, "Error updating floor", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -187,4 +305,3 @@ class ProfileActivity : AppCompatActivity(), CoroutineScope {
         }
     }
 }
-
