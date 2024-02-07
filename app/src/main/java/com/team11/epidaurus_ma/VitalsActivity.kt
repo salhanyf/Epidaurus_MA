96e39483183e98e5e0116e8dfa4fc3680178cc37
David Molina <dav.molina18@gmail.com>
2024-02-07 18:21:49 -0500

    Displaying esp32 data to MA

--- app/src/main/java/com/team11/epidaurus_ma/VitalsActivity.kt
+++ app/src/main/java/com/team11/epidaurus_ma/VitalsActivity.kt
@@ -8,16 +8,18 @@ import android.widget.TextView
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.from
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.coroutines.CoroutineContext
 
 class VitalsActivity : AppCompatActivity(), CoroutineScope {
-
+    private var status: Boolean = true
     private val supabase = createSupabaseClient(
         "https://faafgdjvgcpjbchmbmhg.supabase.co",
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhYWZnZGp2Z2NwamJjaG1ibWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM2MTg4NzAsImV4cCI6MjAxOTE5NDg3MH0.aqg8lSD7tQrWhXjfZi7OiRJOEF1ArG-wdRd9KauvZPU",
@@ -34,6 +36,7 @@ class VitalsActivity : AppCompatActivity(), CoroutineScope {
     override fun onDestroy() {
         super.onDestroy()
         job.cancel()
+        status = false
         launch{
             supabase.close()
         }
@@ -47,6 +50,7 @@ class VitalsActivity : AppCompatActivity(), CoroutineScope {
         setContentView(R.layout.activity_vitals)
 
         val nameTextView = findViewById<TextView>(R.id.NameTextView)
+        val fallValueTextView = findViewById<TextView>(R.id.FallValue)
         var patientId:Int = 0
 
         val backButton = findViewById<ImageView>(R.id.backButton)
@@ -64,11 +68,28 @@ class VitalsActivity : AppCompatActivity(), CoroutineScope {
             nameTextView.text = patientNameText
             Log.d("VitalsActivity", "Patient Name: $patientNameText")
         }
+        launch {
+            while (status) {
+                fallValueTextView.text = supabaseFetch().touched.toString()
+                Log.e("This ain't stoppin","yeah")
+                sleep(2500)
+            }
+        }
     }
 
     private fun getCurrentDateTimeAsString(): String {
         val currentDateTime = LocalDateTime.now()
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
         return currentDateTime.format(formatter)
+    }
+
+    private suspend fun supabaseFetch():CapacitiveTouchResponse{
+        val value = supabase.from("ESP32CapacitiveTouch").select(){
+            filter {
+                eq("id", 1)
+            }
+        }.decodeSingle<CapacitiveTouchResponse>()
+
+        return value
     }
 }
