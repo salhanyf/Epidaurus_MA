bb3ca7ed5c7b35aea57e0938a66872fd0ea961f3
Farah <farah.salhany@hotmail.com>
2024-02-24 12:41:53 -0500

    staying signed in fixed closes #22

--- app/src/main/java/com/team11/epidaurus_ma/MainActivity.kt
+++ app/src/main/java/com/team11/epidaurus_ma/MainActivity.kt
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.gotrue.auth
 
 class MainActivity : AppCompatActivity() {
+    val supabase = createSupabaseClient(
+        "https://faafgdjvgcpjbchmbmhg.supabase.co",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhYWZnZGp2Z2NwamJjaG1ibWhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM2MTg4NzAsImV4cCI6MjAxOTE5NDg3MH0.aqg8lSD7tQrWhXjfZi7OiRJOEF1ArG-wdRd9KauvZPU"
+    ){
+        install(Auth)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkUserSession()
+
         val signUpBtn = findViewById<Button>(R.id.SignUpBtn)
         val signInBtn = findViewById<Button>(R.id.SignInBtn)
 
@@ -20,6 +33,16 @@ class MainActivity : AppCompatActivity() {
         signInBtn.setOnClickListener{
             val signInIntent = Intent (this, LoginActivity::class.java)
             startActivity(signInIntent)
+        }
+    }
+
+    private fun checkUserSession() {
+        val session = supabase.auth.currentSessionOrNull()
+
+        if (session != null) {
+            val homeIntent = Intent(this, HomePageActivity::class.java)
+            startActivity(homeIntent)
+            finish()
         }
     }
 }
