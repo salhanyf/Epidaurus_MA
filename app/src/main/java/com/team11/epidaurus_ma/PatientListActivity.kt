55220423417bb0efa7ce3d60f63541392f214627
Farah <farah.salhany@hotmail.com>
2024-03-17 01:56:08 -0400

    patient search implemented

--- app/src/main/java/com/team11/epidaurus_ma/PatientListActivity.kt
+++ app/src/main/java/com/team11/epidaurus_ma/PatientListActivity.kt
@@ -1,7 +1,13 @@
 package com.team11.epidaurus_ma
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,6 +18,7 @@ import io.github.jan.supabase.gotrue.Auth
 import io.github.jan.supabase.gotrue.auth
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.postgrest.query.filter.TextSearchType
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -55,11 +62,44 @@ class PatientListActivity : AppCompatActivity(), CoroutineScope{
         val nameTextView = findViewById<TextView>(R.id.NameTextView)
         val departmentTextView = findViewById<TextView>(R.id.DepTextView)
         val floorTextView = findViewById<TextView>(R.id.FloorTextView)
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
 
         val backButton = findViewById<ImageView>(R.id.backButton)
         backButton.setOnClickListener {
             finish()
         }
+
+        val searchButton = findViewById<ImageView>(R.id.searchButton)
+        searchButton.setOnClickListener{
+            if (searchEditText.visibility == View.GONE) {
+                searchEditText.visibility = View.VISIBLE
+                searchEditText.requestFocus()
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT)
+            } else {
+                searchEditText.visibility = View.GONE
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+            }
+        }
+
+        searchEditText.addTextChangedListener(object :TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                // Fetch and filter patient list based on search query
+                launch {
+                    val searchQuery = s.toString()
+                    val nurseMetadata = fetchUserData(supabase)
+                    val data = getRows(supabase, nurseMetadata, searchQuery)
+                    val patientList = data.orEmpty()
+                    val recyclerView = findViewById<RecyclerView>(R.id.patientListRecyclerView)
+                    recyclerView.adapter = PatientAdapter(patientList)
+                }
+            }
+        })
 
         launch{
             val nurseMetadata = fetchUserData(supabase)
@@ -88,13 +128,32 @@ class PatientListActivity : AppCompatActivity(), CoroutineScope{
         return metadataJSON
     }
 
-    private suspend fun getRows(supabase: SupabaseClient, metadata:JSONObject?): List<PatientDBResponse>? {
-        val response = supabase.from("Patients").select(){
+//    private suspend fun getRows(supabase: SupabaseClient, metadata:JSONObject?): List<PatientDBResponse>? {
+//        val response = supabase.from("Patients").select(){
+//            filter {
+//                eq("department", metadata?.get("Department ID").toString())
+//                eq("floor", metadata?.get("Floor ID").toString())
+//            }
+//        }.decodeList<PatientDBResponse>()
+//        return response
+//    }
+
+    private suspend fun getRows(supabase: SupabaseClient, metadata:JSONObject?, searchQuery: String? = null): List<PatientDBResponse>? {
+        var response = supabase.from("Patients").select(){
             filter {
                 eq("department", metadata?.get("Department ID").toString())
                 eq("floor", metadata?.get("Floor ID").toString())
             }
-        }.decodeList<PatientDBResponse>()
-        return response
+        }
+        if (!searchQuery.isNullOrBlank()) {
+            response = supabase.from("Patients").select() {
+                filter {
+                    eq("department", metadata?.get("Department ID").toString())
+                    eq("floor", metadata?.get("Floor ID").toString())
+                    textSearch("name", "'$searchQuery$'", TextSearchType.WEBSEARCH)
+                }
+            }
+        }
+        return response.decodeList<PatientDBResponse>()
     }
 }
